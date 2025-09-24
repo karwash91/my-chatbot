4db6acc6a22d3062301564706ee4190eff1317e9
karwash91 <63009556+karwash91@users.noreply.github.com>
2025-09-24 09:29:27 -0400

    Update dynamodb.tf

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -25,20 +25,3 @@ resource "aws_dynamodb_table" "docs_table" {
     projection_type = "ALL"
   }
 }
-
-resource "aws_dynamodb_table" "answers_table" {
-  name         = "my-chatbot-answers"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "session_id"
-  range_key    = "timestamp"
-
-  attribute {
-    name = "session_id"
-    type = "S"
-  }
-
-  attribute {
-    name = "timestamp"
-    type = "S"
-  }
-}
